2b6db93bcd853e89a2e1a635f41a9351639cafad
yejeii <98299007+yejeii@users.noreply.github.com>
2024-10-02 20:55:57 +0900

    mod: url 수정

--- kanban_board/src/APIHandler.jsx
+++ kanban_board/src/APIHandler.jsx
@@ -6,7 +6,7 @@ class APIHandler {
 
   async getCards() {
     // Fetch cards from the server
-    const response = await fetch(apiUrl+'api/cards');
+    const response = await fetch(apiUrl+'/api/cards');
     let datas = await response.json();
     if (datas.length === 0) {
       return null;
@@ -18,7 +18,7 @@ class APIHandler {
   async postCard(cardObj) {
     console.log(cardObj);
     // Post new card to the server
-    const response = await fetch(apiUrl+'api/cards', {
+    const response = await fetch(apiUrl+'/api/cards', {
       method: 'POST',
       body: JSON.stringify({
         id: cardObj.id,
@@ -32,7 +32,7 @@ class APIHandler {
 
   async putCard(cardObj) {
     // Update existing card
-    await fetch(apiUrl+`api/cards/${cardObj.id}`, {
+    await fetch(apiUrl+`/api/cards/${cardObj.id}`, {
       method: 'PUT',
       body: JSON.stringify({
         id: cardObj.id,
@@ -44,7 +44,7 @@ class APIHandler {
 
   async deleteCard(id) {
     // Delete card by ID
-    await fetch(apiUrl+`api/cards/${id}`, {
+    await fetch(apiUrl+`/api/cards/${id}`, {
       method: 'DELETE',
       body: JSON.stringify({
         id: id
