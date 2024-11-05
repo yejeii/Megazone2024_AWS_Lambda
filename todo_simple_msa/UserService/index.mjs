46580490881d60c61bb7198a63951622e03a9deb
yejeii <98299007+yejeii@users.noreply.github.com>
2024-11-05 16:38:30 +0900

    feat: 구글 소셜 로그인 기능 구현

--- todo_simple_msa/UserService/index.mjs
+++ todo_simple_msa/UserService/index.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';          // Local Strategy
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';   // OAuth Stretegy
 import * as User from './users.js';
 
 const app = express();
@@ -9,24 +9,45 @@ app.use(express.json());
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
 
 // 미들웨어 추가
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// Local Strategy 를 통한 로그인 인증 (Passport)
-// 로그인 전략을 실행하고, done()을 호출하면, 다시 passport.authenticate() 라우터로 돌아가 다음 미들웨어를 실행
-passport.use(new LocalStrategy(
-    (username, password, done) => {
-        const user = User.findUser(username);
-        if (!user) {
-            return done(null, false, { message: 'Incorrect username.' });
-        }
+// OAuth Strategy 를 통한 로그인 인증
+passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://www.example.com/auth/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        console.log('google profile : ', profile);
+        try {
+            const exUser = await User.findOne({
+                // 구글 플랫폼에서 로그인 했고 & snsId필드에 구글 아이디가 일치할경우
+                where: { snsId: profile.id, provider: 'google' },
+            });
 
-        if (user.password !== password) {
-            return done(null, false, { message: 'Incorrect password.' });
+            // 이미 가입된 구글 프로필이면 성공
+            if(exUser) {
+                console.log('Existing user:', exUser);
+                done(null, exUser); // 로그인 인증 완료
+            } else {
+                // 가입되지 않은 유저 0 -> 회원가입 & 로그인
+                const newUser = await User.create({
+                    email: profile?.email[0].value,
+                    nick: profile.displayName,
+                    snsId: profile.id,
+                    provider: 'google',
+                });
+                console.log('New user:', newUser);
+                done(null, newUser);
+            }
+        } catch (e) {
+            console.error(e);
+            done(e);
         }
-        return done(null, user);
     }
 ));
+
 
 // Object -> JSON
 // 인증 후 사용자 정보를 세션에 저장
@@ -43,34 +64,19 @@ passport.deserializeUser((id, done) => {
     done(null, user);
 });
 
-// Local Strategy
-app.post('/login', passport.authenticate('local'), (req, res) => {
-    res.json({ message: 'Logged in successfully' });
-});
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/users', (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-    res.json(User.users);
-});
-
-app.get('/logout', (req, res, next) => {
-    // passport 정보 삭제
-    req.logOut(err => {
-        if(err) {
-            return next(err);
-        } else {
-            // 서버측 세션 삭제
-            req.session.destroy(() => {
-                // 클라이언트 측 세션 암호화 쿠키 삭제
-                res.cookie('connect.sid', '', {maxAge: 0});
-                return res.status(201).json({message: 'Logout successfully!'});
-            });
-        }
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
     });
-})
 
+
+// 브라우저에서 http://localhost:3001/auth/google 로 접속
 app.listen(3001, () => {
     console.log('UserService is running on port 3001');
 });
--- todo_simple_msa/UserService/users.js
+++ todo_simple_msa/UserService/users.js
@@ -1,6 +1,6 @@
 export const users = [
-    { id: 1, username: 'user1', password: 'password1'},
-    { id: 2, username: 'user2', password: 'password2'}
+    { id: 1, email: 'user1@example.com', password: 'password1', nick: 'User1', snsId: null, provider: null },
+    { id: 2, email: 'user2@example.com', password: 'password2', nick: 'User2', snsId: null, provider: null }
 ];
 
 export function findUser(username) {
@@ -9,4 +9,18 @@ export function findUser(username) {
 
 export function findUserById(id) {
     return users.find(user => user.id === id);
+}
+
+export function findOne(criteria) {
+    return users.find(user => {
+        // 모든 주어진 조건이 일치하는 경우에만 반환
+        return Object.keys(criteria).every(key => user[key] === criteria[key]);
+    });
+}
+
+export function create(newUser) {
+    const id = users.length ? users[users.length - 1].id + 1 : 1; // 새로운 ID 생성
+    const user = { id, ...newUser };
+    users.push(user); // 배열에 새 사용자 추가
+    return user;
 }
